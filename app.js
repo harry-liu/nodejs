87d6759047f42ec793fd8f04aca149da0a6ceb16
吴飞 <wufei@wufeideMBP.lan>
2016-11-17 14:28:35 +0800

    1.0

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Koa = require('koa');
-const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
-
+const controller = require('./controller');
 
 const app = new Koa();
 
@@ -11,32 +10,10 @@ app.use(async (ctx, next) => {
     await next();
 });
 
-// add url-route:
-router.get('/hello/:name', async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
-
-router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>';
-});
-
-router.post('/signin', async (ctx, next) => {
-    var name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
-    }
-});
-
 
 app.use(bodyParser());
 // add router middleware:
-app.use(router.routes());
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
